cc12e54cc548591acfdd79f9471adfedb75d656d
hazelnutics <samirrinki@gmail.com>
2020-11-27 18:20:08 +0530

    scheduler update

--- yacs/master.py
+++ yacs/master.py
@@ -1,12 +1,13 @@
 """Master node
-
 This program runs on the master.
 """
 from threading import Thread
-from queue import SimpleQueue
+from queue import Queue
 from enum import Enum
 import socket
 import json
+import random
+import time
 
 class Status(Enum):
     SUCCESS = 1
@@ -17,15 +18,74 @@ class Status(Enum):
 class Scheduler:
     @staticmethod
     def random_scheduler(tasks, workers):
-        pass
+        total_free_slots=0
+        num=0
+        for w in workers:
+            num+=1
+            total_free_slots+=w['free_slot_count']
+        task_mapping = []
+            
+        for task in tasks:
+            while(total_free_slots==0):
+                time.sleep(1)
+
+            if(total_free_slots > 0):
+                if task['depends'] == []:
+                    x = random.randrange(0,num)
+                    if workers[x]['slots'] > 0:
+                        task['wid'] = workers[x]['worker_id']
+                        workers[x]['free_slot_count']-=1
+                        task_mapping.append(task)
+                        total_free_slots -= 1
+        return task_mapping
 
     @staticmethod
     def round_robin_scheduler(tasks, workers):
-        pass
+        total_free_slots=0
+        num=0
+        for w in workers:
+            num+=1
+            total_free_slots+=w['free_slot_count']
+        task_mapping = []
+        c = 0
+        
+        for task in tasks:
+            while(total_free_slots==0):
+                time.sleep(1)
+
+            if total_free_slots > 0 :
+                if task.depends_on == []:
+                        if workers[c]['slots'] > 0:
+                            task['wid'] = workers[c]['worker_id']
+                            workers[c]['free_slot_count']-=1
+                            total_free_slots -= 1
+                            task_mapping.append(task)
+                        c = (c+1)%num
+        return task_mapping
 
     @staticmethod
     def least_loaded_scheduler(tasks, workers):
-        pass
+        total_free_slots=0
+        num=0
+        for w in workers:
+            num+=1
+            total_free_slots+=w['free_slot_count']
+        task_mapping = []
+            
+        for task in tasks:
+            while(total_free_slots==0):
+                time.sleep(1)
+
+            if(total_free_slots > 0):
+                if task['depends'] == []:
+                    y=list(map(lambda x: x['free_slot_count'], workers))
+                    x=y.index(max(y))
+                    if workers[x]['slots'] > 0:
+                        task['wid'] = workers[x]['worker_id']
+                        workers[x]['free_slot_count']-=1
+                        task_mapping.append(task)
+                        total_free_slots -= 1
+        return task_mapping
 
 
 class Master:
@@ -39,19 +99,20 @@ class Master:
         self.scheduler = scheduler
         self.threads = []
         self.connections = []
-        self.job_pool = SimpleQueue()
-        self.task_pool = SimpleQueue()
-
+        self.job_pool = Queue()
+        #self.task_pool = SimpleQueue()
         # threads and connections
         self.request_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.worker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
     # For context manager, establishes the connections and launches them in a new thread
     def __enter__(self):
         # conn_info is just a tuple of (conn, address) that comes from sock.accept()
-        request_conn_info = self.establish_connection(self.request_sock)
-        worker_conn_info = self.establish_connection(self.worker_sock)
-        self.connections.extend([request_conn_info, worker_conn_info])
+        self.request_conn_info = self.establish_connection(self.request_sock,1)
+        self.worker_conn_info = self.establish_connection(self.worker_sock,0)
+
+        self.connections.extend([self.request_conn_info, self.worker_conn_info])
         self.request_thread = Thread(target=self.request_listener, \
                                      args=(self.request_conn_info,))
         self.request_thread.start()
@@ -70,11 +131,50 @@ class Master:
         for thread in self.threads:
             thread.join()
 
-    def request_listener(self, conn):
-        pass
+    def establish_connection(self,s,flag):
+        if(flag):
+            port=5000#requests
+        else:
+            port=5001#workers
+            
+        s.bind(('',port))
+        s.listen(5)
+        return s.accept()
 
-    def worker_listener(self, conn):
-        pass
+    def request_listener(self, conn):
+        data = {'job_id': '0',
+                'map_tasks': [{'task_id': '0_M0', 'duration': 3}, {'task_id': '0_M1', 'duration': 3}, {'task_id': '0_M2', 'duration': 2}],
+                'reduce_tasks': [{'task_id': '0_R0', 'duration': 3}, {'task_id': '0_R1', 'duration': 3}]}
+        req = json.load(data)
+        job = {}
+        job['id'] = req['job_id']
+        job['tasks'] = []
+
+        for red in data['reduce_tasks']:
+            task={}
+            task['job_id']:job['id']
+            task['task_id']:red['task_id']
+            task['duration']:red['duration']
+            task['wid']:None
+            task['depends']:0
+            task['satisfies']:1
+            task['status']:Status(2)
+            job['tasks'].append(task)
+
+        for mapper in data['reduce_tasks']:
+            task={}
+            task['job_id']:job['id']
+            task['task_id']:mapper['task_id']
+            task['duration']:mapper['duration']
+            task['wid']:None
+            task['depends']:1
+            task['satisfies']:0
+            task['status']:Status(2)
+            job['tasks'].append(task)
+        self.job_pool.put(job)
+        
+
+    #def worker_listener(self, conn):
 
     def update_worker_params(self, worker):
         pass
@@ -91,5 +191,5 @@ class Master:
 if __name__ == '__main__':
     # Context management protocol, all threads and connections
     # are automatically closed after it's done
-    with Master() as master:
+    with Master("path/to/config","round_robin_sheduler") as master:
         master.run()
